edeb4b10ccd99b9710e695c9a7afc362f12e2207
Jake <jmckinnon99@gmail.com>
2016-02-23 22:36:29 -0800

    got editing/deleting/class creating working on home page.  Now need to hook together with login and feed.  After that, likes ez.

--- homeController.js
+++ homeController.js
@@ -19,7 +19,9 @@ cs194hApp.controller('HomeController', ['$scope', function($scope) {
    // UB4KnOZOoJ = Mr.Trump's TeacherID
    //password: 2 ,email: 2@test.com
    var myID = 'UB4KnOZOoJ';
+   var myName = 'Mr. Trump';
 
+   var loadClasses = function() {
    var Class = Parse.Object.extend("Class");
    var classesQuery = new Parse.Query(Class);
    classesQuery.equalTo("TeacherID", myID);
@@ -43,6 +45,8 @@ cs194hApp.controller('HomeController', ['$scope', function($scope) {
            alert("Error: " + error.code + " " + error.message);
        }
    });
+   };
+   loadClasses();
    $scope.randPic = function() {
       var pict = 1;
       return "img/" + pict + ".jpg";
@@ -56,28 +60,93 @@ cs194hApp.controller('HomeController', ['$scope', function($scope) {
    $scope.formMessage = "Create a New Class";
 
    $scope.addingClass = false;
-   $scope.classCode;
-   $scope.className;
+
+   $scope.create = {};
+   $scope.create.classCode = 0;
+   $scope.create.nameClass = '';
+
+   $scope.cancelCreate = function() {
+      $scope.editClassIs = undefined;
+      $scope.create.classCode = '';
+      $scope.create.nameClass = '';
+      $scope.addingClass = false;
+   }
+
    $scope.createClass = function() {
       if($scope.editingClass) {
          //change name of existing class
-         console.log($scope.editClassIs);
-
-
+         if($scope.create.nameClass !== ''){
+            console.log($scope.editClassIs);
+            var classObj = $scope.editClassIs;
+            $scope.editClassIs.parseObject.set("ClassName", $scope.create.nameClass);
+            $scope.editClassIs.parseObject.save(null, {
+               success: function(objClass) {
+                  console.log("updated Successfully")
+                  classObj.name = objClass.get("ClassName");
+                  $scope.$apply(); //should update view
+               }
+            });
+         }
          $scope.editClassIs = undefined;
       }else {
-         //add class
+         if(($scope.create.nameClass !== '') && ($scope.create.classCode !== 0)) {
+            //add class
+            console.log($scope.create.classCode);
+            console.log($scope.create.nameClass);
+            var newClass = new Parse.Object("Class");
+            newClass.set("ClassCode", $scope.create.classCode);
+            newClass.set("TeacherName", myName);
+            newClass.set("TeacherID", myID);
 
+            newClass.set("ClassName", $scope.create.nameClass);
+
+
+            newClass.save(null, {
+               success: function(newClass) {
+                  loadClasses();
+               },
+               error: function(newClass, error) {
+                  console.log("error on signin");
+                  alert("Error: " + error.code + " " + error.message);
+               }
+            });
+
+
+         }
       }
 
+      $scope.create.classCode = '';
+      $scope.create.nameClass = '';
       $scope.addingClass = false;
    }
+
+   $scope.deleteClass = function() {
+      console.log($scope.editClassIs);
+      console.log("delete^");
+      $scope.editClassIs.parseObject.destroy({
+         success: function(myObject) {
+            console.log("destroyed!");
+            loadClasses();
+         },
+         error: function(myObject, error) {
+            console.log("no destroy :(");
+            alert("No Delete: error: " + error.code + " " + error.message);
+         }
+      });
+      $scope.cancelCreate();
+
+   }
+
+   $scope.createMessage = "Create Class";
+
    $scope.editClassIs = undefined;
    $scope.editingClass = true;
    $scope.editClass = function(classClicked) {
       $scope.editClassIs = classClicked;
+      $scope.create.nameClass = classClicked.name;
       $scope.editingClass = true;
       $scope.formMessage = "Edit Class Name";
+      $scope.createMessage = "Save Changes";
       $scope.addingClass = true;
    }
 
@@ -85,6 +154,7 @@ cs194hApp.controller('HomeController', ['$scope', function($scope) {
    $scope.newClass = function() {
       console.log('new class');
       $scope.formMessage = "Create a New Class";
+      $scope.createMessage = "Create Class";
       $scope.editingClass = false;
       $scope.addingClass = true;
    }
@@ -97,6 +167,27 @@ cs194hApp.controller('HomeController', ['$scope', function($scope) {
    $scope.logOut = function() {
       console.log("out");
    }
+
+
+   /*
+   var newClass = new Parse.Object(Class);
+         newClass.set("ClassCode", );
+         newClass.set("TeacherName", $scope.main.password);
+         newClass.set("TeacherID", $scope.main.emailAddr);
+
+         newClass.set("ClassName", $scope.main.firstName);
+
+
+         newClass.save(null, {
+            success: function(newClass) {
+               loadClasses();
+            },
+            error: function(newClass, error) {
+               console.log("error on signin");
+               alert("Error: " + error.code + " " + error.message);
+            }
+         });
+*/
 
 }]);
 
